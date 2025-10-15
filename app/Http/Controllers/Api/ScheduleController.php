be14e0addfb5b0639513fb137dd6d80d17f35159
Luke Frost <lukefrost96@hotmail.co.uk>
2025-10-15 19:27:32 +0100

    Added api controller for the schedule entity

---
+++ app/Http/Controllers/Api/ScheduleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\ScheduleService;
+use Illuminate\Http\Request;
+
+class ScheduleController extends Controller
+{
+    protected $service;
+
+    public function __construct(ScheduleService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(Request $request): \Illuminate\Http\JsonResponse
+    {
+        return response()->json($this->service->getSchedules($request));
+    }
+
+    public function show($id): \Illuminate\Http\JsonResponse
+    {
+        return response()->json($this->service->getSchedule($id));
+    }
+
+    public function saveStatus($id): \Illuminate\Http\JsonResponse
+    {
+        return response()->json($this->service->saveScheduleStatus($id));
+    }
+}
