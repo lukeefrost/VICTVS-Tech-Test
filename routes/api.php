26a56f75262b4a12499758fb2bbbb069bbd3d2b4
Luke Frost <lukefrost96@hotmail.co.uk>
2025-10-15 19:24:46 +0100

    Added new api routes

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\ScheduleController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/schedules', [ScheduleController::class, 'index']);
+Route::get('/schedules/{id}', [ScheduleController::class, 'show']);
+Route::patch('/schedules/{id}/advance', [ScheduleController::class, 'saveStatus']);
