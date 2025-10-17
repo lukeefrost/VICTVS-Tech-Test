470003e3a68d5aa574bacff21a19012759f2e038
Luke Frost <lukefrost96@hotmail.co.uk>
2025-10-17 14:31:49 +0100

    Added SPA path route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ Route::get('dashboard', function () {
 
 Route::get('/schedules', [ScheduleController::class, 'index'])->name('schedules.index');
 
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
